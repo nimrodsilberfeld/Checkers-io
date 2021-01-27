cee8feb351ad35c7546e38b20ba1f23f45dd2ab4
nimrodsilberfeld <nimrod513@gmail.com>
2021-01-27 21:02:36 +0200

    init6

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const express = require('express')
 const path = require('path')
 const socketio = require('socket.io')
-const port = 3000
+const port =process.env.PORT|| 3000
 require('./db/mongoose')
 const userRouter = require('./route/userRoute')
 const { addUser, getAllConnectedUsers, getUser, removeUser, getUsersInRoom } = require('./utils/users')
